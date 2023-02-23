d20c1996eef0c75fa2f151195e78b28a1a190f3c
unknown <mirvatbarakat1@gmail.com>
2023-02-23 18:00:38 +0200

    Declaring Variables

---
+++ game.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event){
+    var start = document.getElementById("start");
+    var end = document.getElementById("end");
+    var status = document.getElementById("status");
+    var boundaries = document.querySelectorAll(".boundary");
+    var score = 0;
+    
+});
