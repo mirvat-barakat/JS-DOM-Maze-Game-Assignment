a679358fdf013070216908ab427a35470139ebbd
unknown <mirvatbarakat1@gmail.com>
2023-02-23 21:37:05 +0200

    Adding reset function

--- game.js
+++ game.js
@@ -9,5 +9,18 @@ document.addEventListener("DOMContentLoaded", function(event){
     for (var i = 0; i < boundaries.length; i++) {
         boundaries[i].addEventListener("mouseover", lostGame);
     }
+    function resetGame() {
+        for (var i = 0; i < boundaries.length; i++) {
+            boundaries[i].classList.remove("youlose");
+        }
+        status.textContent = "Begin by moving your mouse over the 'S'.";
+        score = 0;
+    }
+    function wonGame() {
+        if (score == 0 && !start.classList.contains("youlose")) {
+            status.textContent = "You win! Click the 'S' to play again.";
+            score += 5;
+        }
+    }
 
 });
