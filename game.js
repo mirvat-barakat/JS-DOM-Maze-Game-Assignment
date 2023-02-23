0d59c06166c480410b73c80c4fc7d420e1d38fe9
unknown <mirvatbarakat1@gmail.com>
2023-02-23 19:28:47 +0200

    Adding event listners for start, end , and boundaries

--- game.js
+++ game.js
@@ -4,5 +4,10 @@ document.addEventListener("DOMContentLoaded", function(event){
     var status = document.getElementById("status");
     var boundaries = document.querySelectorAll(".boundary");
     var score = 0;
-    
+    start.addEventListener("mouseover", resetGame);
+    end.addEventListener("mouseover", wonGame);
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].addEventListener("mouseover", lostGame);
+    }
+
 });
